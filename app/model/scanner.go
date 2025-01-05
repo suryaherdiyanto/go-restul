4778911cdf5255d6e01c3754fad4d17f0006a7e3
suryaherdiyanto <surya@bwisemedia.com>
2025-01-05 17:31:05 +0800

    Feat: fix scanner on test

--- app/model/scanner.go
+++ app/model/scanner.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
 	"reflect"
 
 	"github.com/go-restful/helper"
@@ -15,14 +16,25 @@ func ScanRow(d interface{}, rows *sql.Rows) {
 	}
 
 	ref := reflect.TypeOf(d)
+	if ref.Kind() == reflect.Ptr {
+		ref = ref.Elem()
+	}
+
+	if ref.Kind() != reflect.Struct {
+		panic(fmt.Errorf("model.ScanRow only accepts struct, kind: %v", ref.Kind()))
+	}
+
+	var dRefs []interface{}
 
 	for i := 0; i < ref.NumField(); i++ {
 		for _, column := range columns {
 			f := ref.Field(i)
 
 			if f.Tag.Get("db") == column {
-				rows.Scan(reflect.ValueOf(f).Addr())
+				dRefs = append(dRefs, reflect.ValueOf(d).Elem().Field(i).Addr().Interface())
 			}
 		}
 	}
+
+	rows.Scan(dRefs...)
 }
